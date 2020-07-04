f16fe04d5c365bcb526c591dd70f65731b24087b
unknown <lorena20031989@hotmail.com>
2020-07-04 12:31:41 -0500

    files

---
+++ api_keys_all.py
@@ -0,0 +1,14 @@
+#  Newyork Times API
+nyt_api_key = ""
+
+# OpenWeatherMap API Key
+weather_api_key = ""
+
+# Google API Key
+
+google_api_key = "AIzaSyDwyHKQd0p6QDOdOk07iNaoyW74uv4_S_M"
+#google_api_key = ""
+
+
+# Census US Data API Key
+census_api_key = ""
